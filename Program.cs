108cb8feaf13101e3c84d58f0f66f0ce203ea871
Enrique Carbonell Enriquez <enrique_carbonel@hotmail.com>
2024-03-14 14:39:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        string inputFilePath = "input.txt";  // Replace with your input file path
+        string outputFilePath = "output.txt";  // Replace with your output file path
+
+        // Read the string from the input file
+        string s = File.ReadAllText(inputFilePath);
+
+        // Format the string
+        string formattedString = FormatString(s);
+
+        // Write the formatted string to the output file
+        File.WriteAllText(outputFilePath, formattedString);
+
+        Console.WriteLine($"Formatted string written to {outputFilePath}");
+    }
+
+    public static string FormatString(string s)
+    {
+        // Remove periods
+        s = s.Replace(".", "");
+
+        // Add ":" every 2 elements
+        string formattedString = string.Join(":", Enumerable.Range(0, s.Length / 2).Select(i => s.Substring(i * 2, 2)));
+
+        // Convert to uppercase
+        formattedString = formattedString.ToUpper();
+
+        return formattedString;
+    }
+}
---
+++ README.txt
@@ -0,0 +1,11 @@
+Code utilized to format a string of a MAC address that was wrong formatted.
+
+The MAC Address was written as aaaa.0000.aaaa
+
+This code what it does is that it grabs a .txt file with a bunch of MAC Addresses (each on its own separate line) and it converts it to the correct format (AA:AA:AA:AA:AA:AA).
+
+WARNINGS:
+
+ - Be careful about adding whitespace as this will affect the format of the MAC addresses
+ - Only write MAC addresses and nothing else
+ - Must be in the same wrong format as previously mentioned
